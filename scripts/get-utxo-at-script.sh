8695ac851c0c89ef77979d0adb8721da8dbff923
Shan Zhang <yourgitemail@example.com>
2022-06-24 18:45:22 -0400

    shell script to get datum values at script address taking in api key and script adddress as parameters

---
+++ scripts/get-utxo-at-script.sh
@@ -0,0 +1,8 @@
+curl -H "project_id: $1" \
+"https://cardano-testnet.blockfrost.io/api/v0/addresses/$2/utxos" | grep -o '"data_hash": *"[^"]*"' | grep -o '"[^"]*"$' | sed -e 's/^"//' -e 's/"$//' > data_hashs.txt
+
+
+for i in $(cat data_hashs.txt);do
+	content="$(curl -H "project_id: $1" -s "https://cardano-testnet.blockfrost.io/api/v0/scripts/datum/$i")"
+        echo "$content"| sed "/error/d"  >> output.txt
+done
