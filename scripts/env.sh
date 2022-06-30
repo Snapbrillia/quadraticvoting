486c514810f0c53610b77ae0c30a6f61797fd08a
ritterbush <ritterbush@berkeley.edu>
2022-06-30 11:48:52 -0700

    added transaction fn to env.sh

--- scripts/env.sh
+++ scripts/env.sh
@@ -18,4 +18,36 @@ address_build () {
         --out-file $2
 }
 
+# Get tx-in hash address, given an address FILE name (not the address itself)
+get_hash_addr () {
+
+    return `cardano-cli query utxo \
+        --address $(cat $filename3) \
+        --testnet-magic 1097911063 \
+    | awk 'FNR == 3 {print $1}'`
+
+}
+
+# Send lovelace from Arg1 addr to Ar2 addr with amt of Arg3 lovelace using signing key Arg4 (Which should be Arg1's signing key--We could find this automatically without this parameter perhaps)
+# Needs testing, esp. for the fn call below
+send_lovelace () {
+
+cardano-cli transaction build \
+    --alonzo-era \
+    --testnet-magic 1097911063 \
+    --change-address $(cat $1) \
+    --tx-in `get_hash_addr $1`#0 \ # Will this work ????
+    --tx-out "$(cat $2) $3" \
+    --out-file tx.body
+
+cardano-cli transaction sign \
+    --tx-body-file tx.body \
+    --signing-key-file $4 \
+    --testnet-magic 1097911063 \
+    --out-file tx.signed
+
+cardano-cli transaction submit \
+    --testnet-magic 1097911063 \
+    --tx-file tx.signed
+}
 
