3cac500fbb6afec3609209e0a7757ccbe7478ed1
Shan Zhang <yourgitemail@example.com>
2022-06-30 19:06:39 -0400

    added --tx-in before hash

--- scripts/tx-hash-and-index.sh
+++ scripts/tx-hash-and-index.sh
@@ -1,1 +1,1 @@
-cardano-cli query utxo --address $1 --testnet-magic 1097911063 | awk '{ print $1"#"$2}' | sed '/-/d' | sed '1d'
+cardano-cli query utxo --address $1 --testnet-magic $MAGIC | awk '{ print $1"#"$2}' | sed '/-/d' | sed '1d'| sed 's/^/--tx-in /'
