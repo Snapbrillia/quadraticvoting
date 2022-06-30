7b1606946ef9766bbe58a582dd23dbed967227ff
Shan Zhang <yourgitemail@example.com>
2022-06-30 19:32:32 -0400

    added sed to remove newline

--- scripts/env.sh
+++ scripts/env.sh
@@ -84,6 +84,7 @@ cardano-cli transaction submit \
 get_txHash_and_txIndex () {
 
     cardano-cli query utxo --address $1 --testnet-magic $MAGIC \ 
-	| awk '{ print $1"#"$2}' | sed '/-/d' | sed '1d'| sed 's/^/--tx-in /'
+	| awk '{ print $1"#"$2}' | sed '/-/d' | sed '1d' \ 
+	| sed 's/^/--tx-in /' | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/ /g'
 
 }
