66bebc74b5d2569996deebc09dfe443209254152
ritterbush <ritterbush@berkeley.edu>
2022-06-30 18:49:17 -0700

    added send fn from 1 wallet, multiple utxos to multiple wallets

--- scripts/env.sh
+++ scripts/env.sh
@@ -68,7 +68,6 @@ get_hash_addr () {
 }
 
 # Send lovelace from Arg1 addr to Ar2 addr with amt of Arg3 lovelace using signing key Arg4 (Which should be Arg1's signing key--We could find this automatically without this parameter perhaps)
-# Needs testing, esp. for the fn call below
 send_lovelace () {
 
 cardano-cli transaction build \
@@ -99,3 +98,53 @@ get_txHash_and_txIndex () {
         | sed 's/^/--tx-in /' | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/ /g'`
 
 }
+
+
+# Send lovelace from Arg1 addr (including all of its utxos) to a range of Arg2-Arg3 of addresses (generated from generate_keys_addr_hash_range--should be a contigous range if generated separately) of an equal amts of lovelace of Arg4 using signing key Arg5 (Which should be Arg1's signing key--We could find this automatically without this parameter perhaps)
+send_lovelace_from_one_wallet_with_multiple_utxos_to_multiple_wallets () {
+
+    tx_in_str=$(get_txHash_and_txIndex $1)
+    tx_out_str=''
+    signing_keys_str=''
+    num_of_wallets=`expr $3 - $2`
+    lovelace_amt=`expr $4 / $num_of_wallets`
+    lovelace_amt=`expr $lovelace_amt + 1` # Include the spending wallet
+
+    # Potential change: we could query the total amount of lovelace at all utxos of spending wallet instead of relying on Arg4; but the current way provides flexibility of limiting the amount to spend
+
+    # Build the string of --tx-out's
+    for i in $(seq $2 $3)
+    do
+        cat=$(cat $i.addr)
+        tx_out_str=$tx_out_str' --tx-out'$cat'+'$lovelace_amt
+    done
+
+    # Build the string of signing key files
+    for i in $(seq $2 $3)
+    do
+        signing_keys_str=$signing_keys_str' --signing-key-file'$i'.skey'
+    done
+
+    # Transaction
+    cardano-cli transaction build \
+        --alonzo-era \
+        $MAGIC \
+        $tx_in_str \
+        --change-address $(cat $1) \
+        --tx-out $tx_out_str \
+        --out-file multiTx.body
+
+    cardano-cli transaction sign \
+        --tx-body-file multiTx.body \
+        --signing-key-file $5 \
+        $signing_keys_str
+        $MAGIC \
+        --out-file multiTx.signed
+
+    cardano-cli transaction submit \
+        $MAGIC \
+        --tx-file tx.signed
+}
+
+
+
