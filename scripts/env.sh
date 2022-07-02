69579ea2bbb8a4494c54f3f67d8770530d0dc49e
keyanmaskoot <keyanmaskoot@snapbrillia.com>
2022-07-01 16:06:01 -1000

    Rewrote function descriptions in `env.sh`, plus minor formatting refinements.

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,36 +1,72 @@
 #!/bin/bash
 
-# Change for your environment
+# ==== CHANGE THIS VARIABLE ACCORDINGLY ==== #
 #export CARDANO_NODE_SOCKET_PATH=~/Plutus/plutus/plutus-pioneer-program/code/week06/testnet/node.socket
 #export CARDANO_NODE_SOCKET_PATH=~/plutus-pioneer-program/code/week03/testnet/node.socket
 export CARDANO_NODE_SOCKET_PATH=node.socket
+# ========================================== #
 export MAGIC='--testnet-magic 1097911063'
 
-# Generates a key pair.  Needs 2 args: 2 names for the generated vkey and skey files respectively.
+# Generates a key pair.
+#
+# Takes 2 arguments:
+#   1. Verification key output file,
+#   2. Signing key output file.
 key_gen () {
-    cardano-cli address key-gen \
+    cardano-cli address key-gen    \
         --verification-key-file $1 \
         --signing-key-file $2
 }
 
-# Builds an address. Needs 2 args: name of the vkey file and name for the generated address file.
+
+# Creates an address file from a verification file.
+# 
+# Takes 2 arguments:
+#   1. Verification key file,
+#   2. Address output file.
 address_build () {
-    cardano-cli address build \
+    cardano-cli address build              \
         --payment-verification-key-file $1 \
-        $MAGIC \
+        $MAGIC                             \
         --out-file $2
 }
 
-# Generates the public key hash. Needs 2 args: name of the vkey file and name for the generated file.
+
+# Creates a public key hash file from a verification file.
+# 
+# Takes 2 arguments:
+#   1. Verification key file,
+#   2. Public key hash output file.
 key_hash () {
-    cardano-cli address key-hash \
+    cardano-cli address key-hash           \
         --payment-verification-key-file $1 \
         --out-file $2
 }
 
 
-# Generates keys and builds addresses for those keys and then public key hash files from numerical ranges Arg1 to Arg2 (inclusive). This follows the style of the PPP lectures, e.g.: 01.vkey, 01.skey, 01.addr, 01.pkh
-# Please be careful. Let's set a limit on the amount for now.
+# Creates the associated address from a Plutus script File.
+# 
+# Takes 2 arguments:
+#   1. Compiled Plutus script file,
+#   2. Address output file.
+plutus_script_to_address () {
+    cardano-cli address build-script \
+        --script-file $1             \
+        --testnet-magic 1097911063   \
+        --out-file $2
+}
+
+
+# Given a numeric range (inclusive), generates all four files of a wallet
+# (.vkey, .skey, .addr, .pkh) for each number.
+#
+# This function has builtin safety to prevent rewrites and wallet loss.
+#
+# For now, the maximum number of generated wallets is capped at 100.
+# 
+# Takes 2 arguments:
+#   1. Starting number,
+#   2. Ending number.
 generate_keys_addr_hash_range () {
 
     max_amt=100
@@ -60,55 +96,49 @@ generate_keys_addr_hash_range () {
     fi
 }
 
-# Send lovelace from Arg1 addr to Ar2 addr with amt of Arg3 lovelace using signing key Arg4 (Which should be Arg1's signing key--We could find this automatically without this parameter perhaps)
-send_lovelace () {
 
-tx_in=`cardano-cli query utxo \
-        --address $(cat $1) \
-        $MAGIC \
-        | awk 'FNR == 3 {print $1}'`'#0'
-
-cardano-cli transaction build \
-    --alonzo-era \
-    $MAGIC \
-    --change-address $(cat $1) \
-    --tx-in $tx_in \
-    --tx-out "$(cat $2) $3 lovelace" \
-    --out-file tx.body
-
-cardano-cli transaction sign \
-    --tx-body-file tx.body \
-    --signing-key-file $4 \
-    $MAGIC \
-    --out-file tx.signed
-
-cardano-cli transaction submit \
-    $MAGIC \
-    --tx-file tx.signed
-}
-
-# Get all utxos from address Arg1
+# Returns a list of all UTxO's available at the given wallet address file, each
+# prefixed with "--tx-in" for convenient use while constructing a transaction.
+# 
+# Takes 1 argument:
+#   1. Wallet address file.
 get_utxos () {
-    echo `cardano-cli query utxo \
-        --address $(cat $1) \
-        $MAGIC \
-        | sed 1,2d | awk '{ print $1"#"$2}' \
-        | sed 's/^/--tx-in /' | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/ /g'`
+    echo `cardano-cli query utxo                      \
+        --address $(cat $1)                           \
+        $MAGIC                                        \
+        | sed 1,2d                                    \
+        | awk '{print $1"#"$2}'                       \
+        | sed 's/^/--tx-in /'                         \
+        | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/ /g'`
 }
 
 
-# Send lovelace from Arg1 addr (including all of its utxos) to a range of Arg2-Arg3 of addresses (generated from generate_keys_addr_hash_range--should be a contigous range if generated separately) of an equal amts of lovelace of Arg4 using signing key Arg5 (Which should be Arg1's signing key--We could find this automatically without this parameter perhaps)
+# Distributes a given total Lovelace count from a wallet, between a number of
+# wallets designated with a numeric range, equally.
+#
+# Consumes all the available UTxO's, and returns the change back to the same
+# wallet.
+#
+# TODO: Might require fixing as it can't handle UTxO's carrying tokens.
+#
+# Takes 5 arguments:
+#   1. The spending wallet,
+#   2. Starting number of the receiving wallets,
+#   3. Ending number of the receiving wallets,
+#   4. Total amount of Lovelace to be distributed equally,
+#   5. Signing key file of the spending wallet. # TODO: Can this be removed?
 send_lovelace_from_one_wallet_with_multiple_utxos_to_multiple_wallets () {
 
     tx_in_str=$(get_utxos $1)
     tx_out_str=''
-    signing_keys_str=''
     num_of_wallets=`expr $3 - $2`
-    num_of_wallets=`expr $num_of_wallets + 1` # +1 for spending wallet and +1 due to inclusive range
+    num_of_wallets=`expr $num_of_wallets + 1` # +1 to compensate range inclusivity.
     lovelace_amt=`expr $4 / $num_of_wallets`
     echo $lovelace_amt $num_of_wallets
 
-    # Potential change: we could query the total amount of lovelace at all utxos of spending wallet instead of relying on Arg4; but the current way provides flexibility of limiting the amount to spend
+    # Potential change: we could query the total amount of lovelace at all
+    # UTxO's of spending wallet instead of relying on Arg4; but the current
+    # way provides flexibility of limiting the amount to spend
 
     # Build the string of --tx-out's
     for i in $(seq $2 $3)
@@ -117,34 +147,38 @@ send_lovelace_from_one_wallet_with_multiple_utxos_to_multiple_wallets () {
         tx_out_str=$tx_out_str' --tx-out '$cat'+'$lovelace_amt
     done
   
-    # Build the string of signing key files
-    for i in $(seq $2 $3)
-    do
-        signing_keys_str=$signing_keys_str' --signing-key-file '$i'.skey'
-    done
-    echo $tx_in_str $tx_out_str $signing_keys_str
+    echo $tx_in_str $tx_out_str
     # Transaction
-    cardano-cli transaction build \
-        --alonzo-era \
-        $MAGIC \
-        $tx_in_str \
-        --change-address $(cat $1) \
-        $tx_out_str \
+    cardano-cli transaction build   \
+        --alonzo-era                \
+        $MAGIC                      \
+        $tx_in_str                  \
+        --change-address $(cat $1)  \
+        $tx_out_str                 \
         --out-file multiTx.body
 
-    cardano-cli transaction sign \
+    cardano-cli transaction sign    \
         --tx-body-file multiTx.body \
-        --signing-key-file $5 \
-        $MAGIC \
+        --signing-key-file $5       \
+        $MAGIC                      \
         --out-file multiTx.signed
 
-    cardano-cli transaction submit \
-        $MAGIC \
+    cardano-cli transaction submit  \
+        $MAGIC                      \
         --tx-file multiTx.signed
 }
 
 
-# Send all lovelace from a range of Arg1-Arg2 of addresses (generated from generate_keys_addr_hash_range--should be a contigous range if generated separately) to Arg3 address.
+# Drains a range of wallets into a single wallet. The receiving wallet will
+# end up with 2 UTxO's: One holding 1 ADA, while the other holds the rest of
+# the spent Lovelaces.
+#
+# TODO: Might require fixing as it can't handle UTxO's carrying tokens.
+#
+# Takes 3 arguments:
+#   1. Starting number of the spending wallets,
+#   2. Ending number of the spending wallet,
+#   3. Address file of the receiving wallet.
 send_lovelace_from_many_wallet_with_multiple_utxos_to_one_wallet () {
 
     tx_in_str=''
@@ -163,22 +197,22 @@ send_lovelace_from_many_wallet_with_multiple_utxos_to_one_wallet () {
     done
 
     # Transaction
-    cardano-cli transaction build \
-        --alonzo-era \
-        $MAGIC \
-        $tx_in_str \
-        --change-address $(cat $3) \
+    cardano-cli transaction build    \
+        --alonzo-era                 \
+        $MAGIC                       \
+        $tx_in_str                   \
+        --change-address $(cat $3)   \
         --tx-out $(cat $3)'+1000000' \
         --out-file multiTx.body
 
-    cardano-cli transaction sign \
-        --tx-body-file multiTx.body \
-        $signing_keys_str \
-        $MAGIC \
+    cardano-cli transaction sign     \
+        --tx-body-file multiTx.body  \
+        $signing_keys_str            \
+        $MAGIC                       \
         --out-file multiTx.signed
 
-    cardano-cli transaction submit \
-        $MAGIC \
+    cardano-cli transaction submit   \
+        $MAGIC                       \
         --tx-file multiTx.signed
 }
 
