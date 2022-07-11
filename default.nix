90bdaac154fa6933a02c6b19acc9fd51667181fb
Andy Sizer <andysizer@gmail.com>
2022-07-11 16:34:12 +0000

    Initial attempt to update haskell.nix, hackage.nix  and stackage.nix

--- default.nix
+++ default.nix
@@ -8,21 +8,19 @@
 ########################################################################
 
 let
-  # Here a some of the various attributes for the variable 'packages':
-  #
-  # { pkgs
-  #   plutus-starter: {
-  #     haskell: {
-  #       project # The Haskell project created by haskell-nix.project
-  #       packages # All the packages defined by our project, including dependencies
-  #       projectPackages # Just the packages in the project
-  #     }
-  #     hlint
-  #     cabal-install
-  #     stylish-haskell
-  #     haskell-language-server
-  #   }
-  # }
+  
+  hackageSrc = builtins.fetchTarball https://github.com/input-output-hk/hackage.nix/archive/master.tar.gz;
+  stackageSrc = builtins.fetchTarball https://github.com/input-output-hk/stackage.nix/archive/master.tar.gz;
+  haskellSrc = builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz;
+
+  haskellNix = import haskellSrc {
+    # This allows you to override the pins used by `haskell.nix` internally
+    sourcesOverride = {
+      hackage = hackageSrc;
+      stackage = stackageSrc;
+    };
+  };
+  
   packages = import ./nix { inherit source-repo-override; };
 
   inherit (packages) pkgs plutus-starter;
