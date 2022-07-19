8d98286615ecdc47ccb8f8306ded2b9dc6cbaf5c
Andy Sizer <andysizer@gmail.com>
2022-07-19 15:31:52 +0700

    replace shell.nix with that based on cardano-node

--- shell.nix
+++ shell.nix
@@ -1,165 +1,143 @@
-{ system ? builtins.currentSystem
-, enableHaskellProfiling ? false
-, sourcesOverride ? { }
-, sources ? import ./nix/sources.nix { inherit system; } // sourcesOverride
-, packages ? import ./. { inherit system enableHaskellProfiling sources sourcesOverride; }
+let defaultCustomConfig = import ./nix/custom-config.nix defaultCustomConfig;
+# This file is used by nix-shell.
+# It just takes the shell attribute from default.nix.
+in
+{ withHoogle ? defaultCustomConfig.withHoogle
+, profileName ? defaultCustomConfig.localCluster.profileName
+, workbenchDevMode ? defaultCustomConfig.localCluster.workbenchDevMode
+, useCabalRun ? true
+, customConfig ? {
+    inherit withHoogle;
+    localCluster =  {
+      inherit profileName workbenchDevMode;
+    };
+  }
+, pkgs ? import ./nix customConfig
+# to use profiled build of haskell dependencies:
+, profiled ? false
+, cardano-mainnet-mirror ? __getFlake "github:input-output-hk/cardano-mainnet-mirror/nix"
 }:
+with pkgs;
 let
-  #inherit (packages) pkgs plutus-apps plutus-playground pab-nami-demo docs webCommon;
-  inherit (packages) pkgs plutus-apps docs webCommon;
-  inherit (pkgs) stdenv lib utillinux python3 nixpkgs-fmt;
-  inherit (plutus-apps) haskell stylish-haskell sphinxcontrib-haddock sphinx-markdown-tables sphinxemoji nix-pre-commit-hooks;
+  inherit (pkgs) customConfig;
+  inherit (customConfig) withHoogle localCluster;
+  inherit (localCluster) profileName workbenchDevMode;
+  inherit (pkgs.haskell-nix) haskellLib;
+  project = if profiled then cardanoNodeProject.profiled else cardanoNodeProject;
 
-  # Feed cardano-wallet, cardano-cli & cardano-node to our shell.
-  # This is stable as it doesn't mix dependencies with this code-base;
-  # the fetched binaries are the "standard" builds that people test.
-  # This should be fast as it mostly fetches Hydra caches without building much.
-  cardano-wallet = (import sources.flake-compat {
-    inherit pkgs;
-    src = builtins.fetchTree
-      {
-        type = "github";
-        owner = "input-output-hk";
-        repo = "cardano-wallet";
-        rev = "f6d4db733c4e47ee11683c343b440552f59beff7";
-        narHash = "sha256-3oeHsrAhDSSKBSzpGIAqmOcFmBdAJ5FR02UXPLb/Yz0=";
+  ## The default shell is defined by flake.nix: (cardanoNodeProject = flake.project.${final.system})
+  inherit (project) shell;
+
+  ## XXX: remove this once people retrain their muscle memory:
+  dev = project.shell;
+
+  commandHelp =
+    ''
+      echo "
+        Commands:
+          * nix flake lock --update-input <iohkNix|haskellNix> - update nix build input
+          * cardano-cli - used for key generation and other operations tasks
+          * wb - cluster workbench
+          * start-cluster - start a local development cluster
+          * stop-cluster - stop a local development cluster
+          * restart-cluster - restart the last cluster run (in 'run/current')
+                              (WARNING: logs & node DB will be wiped clean)
+      "
+    '';
+  # Test cases will assume a UTF-8 locale and provide text in this character encoding.
+  # So force the character encoding to UTF-8 and provide locale data.
+  setLocale =
+    ''
+      export LANG="en_US.UTF-8"
+    '' + lib.optionalString haveGlibcLocales ''
+      export LOCALE_ARCHIVE="${pkgs.glibcLocales}/lib/locale/locale-archive"
+    '';
+
+  haveGlibcLocales = pkgs.glibcLocales != null && stdenv.hostPlatform.libc == "glibc";
+
+  workbench-shell = with customConfig.localCluster;
+    import ./nix/workbench/shell.nix
+      { inherit pkgs lib haskellLib project;
+        inherit setLocale haveGlibcLocales commandHelp;
+        inherit cardano-mainnet-mirror;
+        inherit profileName workbenchDevMode useCabalRun;
+        inherit profiled withHoogle;
       };
-  }).defaultNix;
-  cardano-node = import
-    (pkgs.fetchgit {
-      url = "https://github.com/input-output-hk/cardano-node";
-      # A standard release compatible with the cardano-wallet commit above is always preferred.
-      rev = "1.34.1";
-      sha256 = "1hh53whcj5y9kw4qpkiza7rmkniz18r493vv4dzl1a8r5fy3b2bv";
-    })
-    { };
 
-  # No thanks
-  # For Sphinx, and ad-hoc usage
-  # sphinxTools = python3.withPackages (ps: [
-  #   sphinxcontrib-haddock.sphinxcontrib-domaintools
-  #   sphinx-markdown-tables
-  #   sphinxemoji
-  #   ps.sphinxcontrib_plantuml
-  #   ps.sphinxcontrib-bibtex
-  #   ps.sphinx
-  #   ps.sphinx_rtd_theme
-  #   ps.recommonmark
-  # ]);
+  devops =
+    let devopsShellParams =
+          { inherit workbenchDevMode profiled;
+            profileName = "devops-bage";
+            withMainnet = false;
+            useCabalRun = false;
+          };
+        cluster = pkgs.supervisord-workbench-for-profile
+          {
+            inherit (devopsShellParams) profileName useCabalRun;
+          };
+    in project.shellFor {
+    name = "devops-shell";
 
-  # No now, thanks. Maybe later.
-  # Configure project pre-commit hooks
-  # pre-commit-check = nix-pre-commit-hooks.run {
-  #   src = (lib.cleanSource ./.);
-  #   tools = {
-  #     stylish-haskell = stylish-haskell;
-  #     nixpkgs-fmt = nixpkgs-fmt;
-  #     shellcheck = pkgs.shellcheck;
-  #   };
-  #   hooks = {
-  #     purs-tidy-hook = {
-  #       enable = true;
-  #       name = "purs-tidy";
-  #       entry = "${plutus-apps.purs-tidy}/bin/purs-tidy format-in-place";
-  #       files = "\\.purs$";
-  #       language = "system";
-  #     };
-  #     stylish-haskell.enable = true;
-  #     nixpkgs-fmt = {
-  #       enable = true;
-  #       # While nixpkgs-fmt does exclude patterns specified in `.ignore` this
-  #       # does not appear to work inside the hook. For now we have to thus
-  #       # maintain excludes here *and* in `./.ignore` and *keep them in sync*.
-  #       excludes = [ ".*nix/pkgs/haskell/materialized.*/.*" ".*/spago-packages.nix$" ];
-  #     };
-  #     shellcheck.enable = true;
-  #     png-optimization = {
-  #       enable = true;
-  #       name = "png-optimization";
-  #       description = "Ensure that PNG files are optimized";
-  #       entry = "${pkgs.optipng}/bin/optipng";
-  #       files = "\\.png$";
-  #     };
-  #   };
-  # };
+    packages = _: [];
 
-  nixFlakesAlias = pkgs.runCommand "nix-flakes-alias" { } ''
-    mkdir -p $out/bin
-    ln -sv ${pkgs.nixFlakes}/bin/nix $out/bin/nix-flakes
-  '';
+    nativeBuildInputs = with cardanoNodePackages; [
+      nixWrapped
+      cardano-cli
+      bech32
+      cardano-ping
+      cardano-cli
+      cardano-node
+      cardano-topology
+      cardano-tracer
+      locli
+      tx-generator
+      pkgs.graphviz
+      python3Packages.supervisor
+      python3Packages.ipython
+      cluster.interactive-start
+      cluster.interactive-stop
+      cluster.interactive-restart
+      cardanolib-py
+      cluster.workbench.workbench
+      pstree
+      pkgs.time
+    ];
 
-  # build inputs from nixpkgs ( -> ./nix/default.nix )
-  nixpkgsInputs = with pkgs; [
-    cacert
-    editorconfig-core-c
-    ghcid
-    jq
-    nixFlakesAlias
-    nixpkgs-fmt
-    nodejs
-    plantuml
-    shellcheck
-    sqlite-interactive
-    stack
-    yq
-    z3
-    zlib
-  ];
+    # Prevents cabal from choosing alternate plans, so that
+    # *all* dependencies are provided by Nix.
+    exactDeps = true;
 
-  # local build inputs ( -> ./nix/pkgs/default.nix )
-  localInputs = (with plutus-apps; [
-    cabal-install
-    cardano-node.cardano-cli
-    cardano-node.cardano-node
-    cardano-wallet.packages.${pkgs.system}.cardano-wallet
-    cardano-repo-tool
-    docs.build-and-serve-docs
-    fixPngOptimization
-    # fix-purs-tidy
-    # fixStylishHaskell
-    haskell-language-server
-    haskell-language-server-wrapper
-    hie-bios
-    hlint
-    #pab-nami-demo.generate-purescript
-    #pab-nami-demo.start-backend
-    #plutus-playground.generate-purescript
-    #plutus-playground.start-backend
-    # psa
-    # purescript-language-server
-    # purs
-    # purs-tidy
-    # spago
-    # spago2nix
-    # stylish-haskell
-    updateMaterialized
-    updateClientDeps
-  ]);
+    shellHook = ''
+      echo "DevOps Tools" \
+      | ${figlet}/bin/figlet -f banner -c \
+      | ${lolcat}/bin/lolcat
+
+      ${workbench-shell.shellHook devopsShellParams}
+
+      # Socket path default to first node launched by "start-cluster":
+      export CARDANO_NODE_SOCKET_PATH=$(wb backend get-node-socket-path ${cluster.stateDir} 'node-0')
+
+      ${setLocale}
+
+      # Unless using specific network:
+      ${lib.optionalString (__hasAttr "network" customConfig) ''
+        export CARDANO_NODE_SOCKET_PATH="$PWD/state-node-${customConfig.network}/node.socket"
+        ${lib.optionalString (__hasAttr "utxo" pkgs.commonLib.cardanoLib.environments.${customConfig.network}) ''
+          # Selfnode and other test clusters have public secret keys that we pull from iohk-nix
+          echo "To access funds use UTXO_SKEY and UTXO_VKEY environment variables"
+          export UTXO_SKEY="${pkgs.commonLib.cardanoLib.environments.${customConfig.network}.utxo.signing}"
+          export UTXO_VKEY="${pkgs.commonLib.cardanoLib.environments.${customConfig.network}.utxo.verification}"
+        ''}
+
+      ''}
+
+      echo "NOTE: you may need to use a github access token if you hit rate limits with nix flake update:"
+      echo '      edit ~/.config/nix/nix.conf and add line `access-tokens = "github.com=23ac...b289"`'
+      ${commandHelp}
+
+    '';
+  };
 
 in
-haskell.project.shellFor {
-  # no sphinxtools thanks.
-  # nativeBuildInputs = nixpkgsInputs ++ localInputs ++ [ sphinxTools ];
-  nativeBuildInputs = nixpkgsInputs ++ localInputs;
-  # We don't currently use this, and it's a pain to materialize, and otherwise
-  # costs a fair bit of eval time.
-  withHoogle = false;
 
-  # Not now - maybe later. Thanks.
-  # shellHook = ''
-  #   ${pre-commit-check.shellHook}
-  # ''
-
-  # Work around https://github.com/NixOS/nix/issues/3345, which makes
-  # tests etc. run single-threaded in a nix-shell.
-  # Sets the affinity to cores 0-1000 for $$ (current PID in bash)
-  # Only necessary for linux - darwin doesn't even expose thread
-  # affinity APIs!
-  shellHook = 
-    lib.optionalString stdenv.isLinux ''
-    ${utillinux}/bin/taskset -pc 0-1000 $$
-  ''
-  + ''
-    export WEB_COMMON_SRC=${webCommon}
-  '';
-}
+ shell // { inherit workbench-shell; inherit devops; inherit dev; }
