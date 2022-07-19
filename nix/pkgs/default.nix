d23227b9d4c24196d66ab503473940e5b08eab47
Andy Sizer <andysizer@gmail.com>
2022-07-19 12:13:12 +0700

    Remove unnecessary packages

--- nix/pkgs/default.nix
+++ nix/pkgs/default.nix
@@ -35,9 +35,7 @@ let
   #
   # dev convenience scripts
   #
-  fix-purs-tidy = pkgs.callPackage ./fix-purs-tidy { inherit purs-tidy; };
   fixStylishHaskell = pkgs.callPackage ./fix-stylish-haskell { inherit stylish-haskell; };
-  fixPngOptimization = pkgs.callPackage ./fix-png-optimization { };
   updateMaterialized = pkgs.writeShellScriptBin "updateMaterialized" ''
     # This runs the 'updateMaterialize' script in all platform combinations we care about.
     # See the comment in ./haskell/haskell.nix
@@ -52,15 +50,6 @@ let
     $(nix-build default.nix -A plutus-apps.haskell.extraPackages.updateAllShaFiles --argstr system x86_64-linux)
     $(nix-build default.nix -A plutus-apps.haskell.extraPackages.updateAllShaFiles --argstr system x86_64-darwin)
   '';
-  updateClientDeps = pkgs.callPackage ./update-client-deps {
-    inherit purs spago spago2nix;
-  };
-
-  #
-  # sphinx python packages
-  #
-  sphinx-markdown-tables = pkgs.python3Packages.callPackage ./sphinx-markdown-tables { };
-  sphinxemoji = pkgs.python3Packages.callPackage ./sphinxemoji { };
 
   # By default pre-commit-hooks.nix uses its own pinned version of nixpkgs. In order to
   # to get it to use our version we have to (somewhat awkwardly) use `nix/default.nix`
@@ -69,27 +58,6 @@ let
     inherit system;
     inherit (sources) nixpkgs;
   });
-
-  # easy-purescript-nix has some kind of wacky internal IFD
-  # usage that breaks the logic that makes source fetchers
-  # use native dependencies. This isn't easy to fix, since
-  # the only places that need to use native dependencies
-  # are deep inside, and we don't want to build the whole
-  # thing native. Fortunately, we only want to build the
-  # client on Linux, so that's okay. However, it does
-  # mean that e.g. we can't build the client dep updating
-  # script on Darwin.
-  easyPS = pkgs.callPackage (sources.easy-purescript-nix) { };
-
-  # We pull out some packages from easyPS that are a pain to get otherwise.
-  # This does mean we can't as easily control the version we get, though.
-  inherit (easyPS) purs-tidy purs spago purescript-language-server psa spago2nix;
-
-  # sphinx haddock support
-  sphinxcontrib-haddock = pkgs.callPackage (sources.sphinxcontrib-haddock) { pythonPackages = pkgs.python3Packages; };
-
-  # ghc web service
-  web-ghc = pkgs.callPackage ./web-ghc { inherit haskell; };
 
   # combined haddock documentation for all public plutus libraries
   plutus-haddock-combined =
@@ -110,23 +78,16 @@ let
     haddock-combine = pkgs.callPackage (sources.plutus-core + "/nix/lib/haddock-combine.nix") { inherit sphinxcontrib-haddock; };
     filterNpm = pkgs.callPackage ../lib/filter-npm.nix { };
     npmlock2nix = pkgs.callPackage sources.npmlock2nix { };
-    buildPursPackage = pkgs.callPackage ../lib/purescript.nix { inherit easyPS; inherit (pkgs) nodejs; };
     buildNodeModules = pkgs.callPackage ../lib/node_modules.nix ({
       inherit npmlock2nix;
-    } // pkgs.lib.optionalAttrs (stdenv.isDarwin) {
-      CoreServices = pkgs.darwin.apple_sdk.frameworks.CoreServices;
-      xcodebuild = pkgs.xcodebuild;
     });
   };
 
 in
 {
-  inherit sphinx-markdown-tables sphinxemoji sphinxcontrib-haddock;
   inherit nix-pre-commit-hooks;
   inherit haskell cabal-install cardano-repo-tool stylish-haskell hlint haskell-language-server haskell-language-server-wrapper hie-bios;
-  inherit purs-tidy purs spago spago2nix purescript-language-server psa;
-  inherit fix-purs-tidy fixStylishHaskell fixPngOptimization updateMaterialized updateClientDeps;
-  inherit web-ghc;
-  inherit easyPS plutus-haddock-combined;
+  inherit fixStylishHaskell updateMaterialized;
+  inherit plutus-haddock-combined;
   inherit lib;
 }
