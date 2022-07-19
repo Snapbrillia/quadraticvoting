9d24a955b569093a164d5b74cb1392547b81f95a
Andy Sizer <andysizer@gmail.com>
2022-07-19 12:44:26 +0700

    Adaptions for quadraticvoting

--- nix/haskell.nix
+++ nix/haskell.nix
@@ -23,10 +23,10 @@ haskell-nix.cabalProject' ({ pkgs
                            , buildProject
                            , ...
                            }: {
-  name = "cardano-node";
+  name = "quadraticvoting";
   src = haskellLib.cleanSourceWith {
     src = ../.;
-    name = "cardano-node-src";
+    name = "quadraticvoting-src";
     filter = name: type: (lib.cleanSourceFilter name type)
       && (haskell-nix.haskellSourceFilter name type)
       # removes socket files
@@ -73,109 +73,16 @@ haskell-nix.cabalProject' ({ pkgs
     in
     [
       ({ pkgs, ... }: {
-        packages.tx-generator.package.buildable = with pkgs.stdenv.hostPlatform; isUnix && !isMusl;
-        packages.cardano-tracer.package.buildable = with pkgs.stdenv.hostPlatform; isUnix && !isMusl;
-        packages.cardano-node-chairman.components.tests.chairman-tests.buildable = lib.mkForce pkgs.stdenv.hostPlatform.isUnix;
-        packages.plutus-tx-plugin.components.library.platforms = with lib.platforms; [ linux darwin ];
-      })
-      ({ pkgs, ... }: {
-        # Needed for the CLI tests.
-        # Coreutils because we need 'paste'.
-        packages.cardano-cli.components.tests.cardano-cli-test.build-tools =
-          lib.mkForce (with pkgs.buildPackages; [ jq coreutils shellcheck ]);
-        packages.cardano-cli.components.tests.cardano-cli-golden.build-tools =
-          lib.mkForce (with pkgs.buildPackages; [ jq coreutils shellcheck ]);
-        packages.cardano-testnet.components.tests.cardano-testnet-tests.build-tools =
-          lib.mkForce (with pkgs.buildPackages; [ jq coreutils shellcheck lsof ]);
-      })
-      ({ pkgs, ... }: {
         # Use the VRF fork of libsodium
         packages = lib.genAttrs [ "cardano-crypto-praos" "cardano-crypto-class" ] (_: {
           components.library.pkgconfig = lib.mkForce [ [ pkgs.libsodium-vrf ] ];
         });
       })
-      ({ pkgs, options, ... }: {
-        # make sure that libsodium DLLs are available for windows binaries,
-        # stamp executables with the git revision, add shell completion, strip/rewrite:
-        packages = lib.mapAttrs
-          (name: exes: {
-            components.exes = lib.genAttrs exes (exe: {
-              postInstall = ''
-                ${setGitRev}
-                ${lib.optionalString (pkgs.stdenv.hostPlatform.isMusl) ''
-                  ${pkgs.buildPackages.binutils-unwrapped}/bin/*strip $out/bin/*
-                ''}
-                ${lib.optionalString (pkgs.stdenv.hostPlatform.isDarwin) ''
-                  export PATH=$PATH:${lib.makeBinPath [ pkgs.haskellBuildUtils pkgs.buildPackages.binutils pkgs.buildPackages.nix ]}
-                  ${pkgs.haskellBuildUtils}/bin/rewrite-libs $out/bin $out/bin/*
-                ''}
-                 ${lib.optionalString (!pkgs.stdenv.hostPlatform.isWindows
-                  && lib.elem exe ["cardano-node" "cardano-cli" "cardano-topology" "locli"]) ''
-                  BASH_COMPLETIONS=$out/share/bash-completion/completions
-                  ZSH_COMPLETIONS=$out/share/zsh/site-functions
-                  mkdir -p $BASH_COMPLETIONS $ZSH_COMPLETIONS
-                  $out/bin/${exe} --bash-completion-script ${exe} > $BASH_COMPLETIONS/${exe}
-                  $out/bin/${exe} --zsh-completion-script ${exe} > $ZSH_COMPLETIONS/_${exe}
-                ''}
-              '';
-            });
-          })
-          projectPackagesExes;
-      })
-      ({ pkgs, config, ... }: {
-        # Packages we wish to ignore version bounds of.
-        # This is similar to jailbreakCabal, however it
-        # does not require any messing with cabal files.
-        packages.katip.doExactConfig = true;
-        # split data output for ekg to reduce closure size
-        packages.ekg.components.library.enableSeparateDataOutput = true;
-        # cardano-cli-test depends on cardano-cli
-        packages.cardano-cli.preCheck = "
-        export CARDANO_CLI=${config.hsPkgs.cardano-cli.components.exes.cardano-cli}/bin/cardano-cli${pkgs.stdenv.hostPlatform.extensions.executable}
-        export CARDANO_NODE_SRC=${src}
-      ";
-        packages.cardano-node-chairman.components.tests.chairman-tests.build-tools =
-          lib.mkForce [
-            pkgs.lsof
-            config.hsPkgs.cardano-node.components.exes.cardano-node
-            config.hsPkgs.cardano-cli.components.exes.cardano-cli
-            config.hsPkgs.cardano-node-chairman.components.exes.cardano-node-chairman
-          ];
-        # cardano-node-chairman depends on cardano-node and cardano-cli
-        packages.cardano-node-chairman.preCheck = "
-        export CARDANO_CLI=${config.hsPkgs.cardano-cli.components.exes.cardano-cli}/bin/cardano-cli${pkgs.stdenv.hostPlatform.extensions.executable}
-        export CARDANO_NODE=${config.hsPkgs.cardano-node.components.exes.cardano-node}/bin/cardano-node${pkgs.stdenv.hostPlatform.extensions.executable}
-        export CARDANO_NODE_CHAIRMAN=${config.hsPkgs.cardano-node-chairman.components.exes.cardano-node-chairman}/bin/cardano-node-chairman${pkgs.stdenv.hostPlatform.extensions.executable}
-        export CARDANO_NODE_SRC=${src}
-      ";
-        # cardano-testnet needs access to the git repository source
-        packages.cardano-testnet.preCheck = "
-        export CARDANO_CLI=${config.hsPkgs.cardano-cli.components.exes.cardano-cli}/bin/cardano-cli${pkgs.stdenv.hostPlatform.extensions.executable}
-        export CARDANO_NODE=${config.hsPkgs.cardano-node.components.exes.cardano-node}/bin/cardano-node${pkgs.stdenv.hostPlatform.extensions.executable}
-        export CARDANO_SUBMIT_API=${config.hsPkgs.cardano-submit-api.components.exes.cardano-submit-api}/bin/cardano-submit-api${pkgs.stdenv.hostPlatform.extensions.executable}
-        ${lib.optionalString (!pkgs.stdenv.hostPlatform.isWindows) ''
-        ''}
-        export CARDANO_NODE_SRC=${src}
-      ";
-      })
-      ({ pkgs, ... }: lib.mkIf (!pkgs.stdenv.hostPlatform.isDarwin) {
-        # Needed for profiled builds to fix an issue loading recursion-schemes part of makeBaseFunctor
-        # that is missing from the `_p` output.  See https://gitlab.haskell.org/ghc/ghc/-/issues/18320
-        # This work around currently breaks regular builds on macOS with:
-        # <no location info>: error: ghc: ghc-iserv terminated (-11)
-        packages.plutus-core.components.library.ghcOptions = [ "-fexternal-interpreter" ];
-      })
       {
         packages = lib.genAttrs projectPackageNames
           (name: { configureFlags = [ "--ghc-option=-Werror" ]; });
       }
-      ({ pkgs, ... }: lib.mkIf pkgs.stdenv.hostPlatform.isLinux {
-        # systemd can't be statically linked
-        packages.cardano-git-rev.flags.systemd = !pkgs.stdenv.hostPlatform.isMusl;
-        packages.cardano-node.flags.systemd = !pkgs.stdenv.hostPlatform.isMusl;
-        packages.cardano-tracer.flags.systemd = !pkgs.stdenv.hostPlatform.isMusl;
-      })
-      # Musl libc fully static build
+      # !!! Musl libc fully static build !!!
       ({ pkgs, ... }: lib.mkIf pkgs.stdenv.hostPlatform.isMusl (
         let
           # Module options which adds GHC flags and libraries for a fully static build
@@ -190,14 +97,5 @@ haskell-nix.cabalProject' ({ pkgs
           doHaddock = false;
         }
       ))
-      ({ pkgs, ... }: lib.mkIf (pkgs.stdenv.hostPlatform != pkgs.stdenv.buildPlatform) {
-        # Remove hsc2hs build-tool dependencies (suitable version will be available as part of the ghc derivation)
-        packages.Win32.components.library.build-tools = lib.mkForce [ ];
-        packages.terminal-size.components.library.build-tools = lib.mkForce [ ];
-        packages.network.components.library.build-tools = lib.mkForce [ ];
-      })
-      # TODO add flags to packages (like cs-ledger) so we can turn off tests that will
-      # not build for windows on a per package bases (rather than using --disable-tests).
-      # configureArgs = lib.optionalString stdenv.hostPlatform.isWindows "--disable-tests";
     ];
 })
