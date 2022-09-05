3edebd9c9f9588b7a02c29bb5ae352093f5a8b1e
Andy Sizer <andysizer@gmail.com>
2022-09-05 14:48:42 +0700

    Get SAM build and SAM local invoke/start-api working
    However this is a temporary solution. It only works for the static
    build.
    
    The dynamic build suffers from the 'dependency' problem.
    
    However I've found an article
    (https://www.tweag.io/blog/2020-09-30-bazel-static-haskell/) which might
    offer a solution which kills all our outstanding 'birds with one stone'.
    
    In case you we're wondering - all of the other 'simple' solutions to
    getting the static build don't work.
    
    But the one above looks promising. It looks like there is a musl ghc
    installation (pre-built) in a nixpkgs revision. If that pans out (i.e.
    there is such an installation and I can get nix to dtrt) then we're
    pretty much done :-) We have a single build that works for dev (SAM) and
    will work as a github action, and it doesn't involve Docker or external
    runners or any of that stuff.

--- build.sh
+++ build.sh
@@ -9,11 +9,17 @@ echo "DRV=${DRV}" 1>&2
 
 rm -f ${RESULT}
 
-nix --quiet build .#${DRV} -o ${RESULT}
+#nix --quiet build .#${DRV} -o ${RESULT}
+nix --quiet build .#qvf-generate-scripts-static.x86_64-linux -o ${RESULT}
+
+# cabal install ${TARGET} --overwrite-policy=always
+
+# rm -f ${ARTIFACTS_DIR}/${TARGET}
+rm -f ${ARTIFACTS_DIR}/*
 
 mkdir -p ${ARTIFACTS_DIR}
-cp $(readlink ${RESULT})/bin/${TARGET} ${ARTIFACTS_DIR}/${TARGET}
 
-rm -f ${RESULT}
+# cp $(readlink ${RESULT})/bin/${TARGET} ${ARTIFACTS_DIR}/bootstrap
+cp ${RESULT}/bin/${TARGET} ${ARTIFACTS_DIR}/bootstrap
 
-# cp template.yaml .aws-sam/build/
+
