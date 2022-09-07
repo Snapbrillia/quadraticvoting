c78456be699c3738bce1cf0686572d1209b6a55a
ritterbush <ritterbush@berkeley.edu>
2022-09-07 11:47:53 -0700

    Registration.hs filled in

--- src/Minter/Registration.hs
+++ src/Minter/Registration.hs
@@ -41,7 +41,7 @@ PlutusTx.unstableMakeIsData ''RegistrationInfo
 
 data RegistrationRedeemer
   = RegisterProject RegistrationInfo
-  | DistributePrize
+  | DistributePrize BuiltinByteString -- ProjectID : hashTxOutRef (riTxOutRef RegistrationInfo)
 
 PlutusTx.makeIsDataIndexed ''RegistrationRedeemer
   [ ('RegisterProject, 0)
@@ -60,8 +60,96 @@ mkRegistrationPolicy :: CurrencySymbol
                      -> Bool
 mkRegistrationPolicy sym tn action ctx =
   -- {{{
-  False -- TODO.
-  -- }}}
+  let
+    info :: TxInfo
+    info = scriptContextTxInfo ctx
+
+    hasInUTxO :: Bool
+    hasInUTxO = any (\i -> isSame (txOutValue $ txInfoResolved i)) $ txInfoInputs info
+
+    hasOutUTxO :: Bool
+    hasOutUTxO = any (\i -> isSame (txOutValue i)) $ txInfoOutputs info
+
+    isSame :: Value -> Bool
+    isSame value = case flattenValue value of
+        [_,(symY', tnY', amtY)] -> symY' == sym && tnY' == tn && amtY == 1
+        _                       -> False
+
+    ownSym :: CurrencySymbol
+    ownSym = ownCurrencySymbol ctx
+
+  in
+  case action of
+    RegisterProject ri ->
+      let
+        signedByRegistrator :: Bool
+        signedByRegistrator = txSignedBy info $ riPubKeyHash ri
+
+        checkTokenName :: TokenName ->  Bool
+        checkTokenName tn = unTokenName tn  == hashTxOutRef (riTxOutRef ri)
+
+        tnIsCorrect :: Bool
+        tnIsCorrect = case flattenValue (txInfoMint info) of
+            [(sym', tn', amt)] -> ownSym == sym' && checkTokenName tn' && amt  == 1 -- ownSym check may be redundant
+            _               -> False
+
+
+        hasRed :: Bool
+        hasRed = any (\i -> hasPTokenandFees (txOutValue i)) $ txInfoOutputs info
+        -- Check red UTXO is present in Output, and that it has P token
+        -- and  min 2 Ada --ref to constant in Utilities Module  is available 
+
+        hasPTokenandFees :: Value -> Bool
+        hasPTokenandFees value = case flattenValue value of
+            [(sym', tn', amt),(symY', tnY', amtY)] -> amt  == minLovelace ||
+                                                      symY' == ownSym && checkTokenName tnY' && amtY == 1
+            _               -> False
+
+        hasUTxO :: Bool
+        hasUTxO = any (\i -> txInInfoOutRef i == riTxOutRef ri) $ txInfoInputs info
+
+
+        {- ---------------------------------------- -}
+
+        -- Go through list of inputs and find "yellow" utxo;
+        -- The yellow one is S (in readme file)
+        -- find :: Foldable t => (a -> Bool) -> t a -> Maybe a
+        -- Plutus Tx version of find.
+        -- Predicate logic involves finding the "yellow"
+
+      in
+        traceIfFalse
+          "Registrator's signature missing"
+          signedByRegistrator                         &&
+        traceIfFalse
+          "Token name is not hash of specified UTXO"
+          tnIsCorrect                                 &&
+        traceIfFalse
+          "Not all tokens present"
+          (hasInUTxO && hasOutUTxO)                   &&
+        traceIfFalse
+          "Missing fees or incorrect token"
+          hasRed                                      &&
+        traceIfFalse
+          "Utxo does not match redeemer"
+          hasUTxO
+
+    DistributePrize  projectID  ->
+    let
+        checkTokenName :: TokenName ->  Bool
+        checkTokenName tn = unTokenName tn  == projectID
+
+        tokenIsCorrect :: Bool
+        tokenIsCorrect = case flattenValue (txInfoMint info) of
+            [(sym', tn', amt)] -> ownSym == sym' && checkTokenName tn' && amt  == (-1) -- ownSym check may be redundant
+            _               -> False
+    in
+        traceIfFalse
+          "Not all tokens present"
+          (hasInUTxO && hasOutUTxO)         &&
+        traceIfFalse
+          "Missing fees or incorrect token"
+          tokenIsCorrect
 
 
 -- TEMPLATE HASKELL, BOILERPLATE, ETC. 
