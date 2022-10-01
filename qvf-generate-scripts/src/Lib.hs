a8872caba6421d3eed25a3cb34d52c07f3160cee
keyanmaskoot <keyanmaskoot@snapbrillia.com>
2022-10-01 16:21:48 +0200

    Update `qvf-generate-scripts`.
    Implemented minor changes to `qvf-generate-scripts` to make it
    compatible with the new module structure.

--- qvf-generate-scripts/src/Lib.hs
+++ qvf-generate-scripts/src/Lib.hs
@@ -12,7 +12,7 @@ import           Ledger               ( PubKeyHash
                                       , TokenName )
 import qualified Ledger.Typed.Scripts as Scripts
 
-import           Datum                ( QVFDatum(RegisteredProjectsCount) )
+import           Data.Datum           ( QVFDatum(RegisteredProjectsCount) )
 
 import qualified QVF                  as OC
 import qualified Minter.Donation      as Don
@@ -28,26 +28,31 @@ data GenerateScriptsParams = GenerateScriptsParams
   deriving (Generic, FromJSON, ToJSON)
 
 data GenerateScriptsResponse = GenerateScriptsResponse
-  { validator     :: Scripts.Validator
-  , mintingPolicy :: Scripts.MintingPolicy
-  , unitRedeemer  :: ()
-  , initialDatum  :: QVFDatum
+  { validator          :: Scripts.Validator
+  , governancePolicy   :: Scripts.MintingPolicy
+  , registrationPolicy :: Scripts.MintingPolicy
+  , donationPolicy     :: Scripts.MintingPolicy
+  , unitRedeemer       :: ()
+  , initialDatum       :: QVFDatum
   } 
   deriving (Generic, FromJSON, ToJSON)
 
 handler :: GenerateScriptsParams -> Context () -> IO (Either String GenerateScriptsResponse)
-handler GenerateScriptsParams {..} _ = 
-  return $ Right $ GenerateScriptsResponse validator mintingPolicy () initialDatum
+handler GenerateScriptsParams{..} _ = 
+  return $ Right GenerateScriptsResponse{..}
   where 
-    currencySymbol = Gov.qvfSymbol txRef deadline
-    regSymbol      = Reg.registrationSymbol currencySymbol
-    donSymbol      = Don.donationSymbol regSymbol
-    qvfParams      = OC.QVFParams
-                      { OC.qvfKeyHolder      = keyHolderPubKeyHash
-                      , OC.qvfSymbol         = currencySymbol
-                      , OC.qvfProjectSymbol  = regSymbol
-                      , OC.qvfDonationSymbol = donSymbol
-                      }
-    validator = OC.qvfValidator qvfParams
-    mintingPolicy = Gov.qvfPolicy txRef deadline
-    initialDatum  = RegisteredProjectsCount 0
+    qvfSymbol = Gov.qvfSymbol txRef deadline
+    regSymbol = Reg.registrationSymbol qvfSymbol
+    donSymbol = Don.donationSymbol regSymbol
+    qvfParams = OC.QVFParams
+                 { OC.qvfKeyHolder      = keyHolderPubKeyHash
+                 , OC.qvfSymbol         = qvfSymbol
+                 , OC.qvfProjectSymbol  = regSymbol
+                 , OC.qvfDonationSymbol = donSymbol
+                 }
+    validator          = OC.qvfValidator qvfParams
+    governancePolicy   = Gov.qvfPolicy txRef deadline
+    registrationPolicy = Reg.registrationPolicy qvfSymbol
+    donationPolicy     = Don.donationPolicy regSymbol
+    unitRedeemer       = ()
+    initialDatum       = RegisteredProjectsCount 0
