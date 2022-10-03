aef723fd3e5ca8cd84b77c2a5e0b00553eac822b
Andy Sizer <andysizer@gmail.com>
2022-10-03 14:02:10 +0700

    Use image from docker hub to build for SAM

--- sam-build.sh
+++ sam-build.sh
@@ -18,7 +18,7 @@ ARTIFACT_DIR=.aws-sam/build/${FUNCTION}
 BUILD_DIR=$(mktemp -d -p . .aws-sam.XXXXXXXX)
 
 docker run -t -v `pwd`:/home/builder/repo -v `pwd`/.cabal.docker:/home/builder/.cabal \
-			   -w /home/builder/repo haskell-cabal-build \
+			   -w /home/builder/repo tyutchev/haskell-cabal-build \
 			   ./cabal-build-function.sh ${BUILD_DIR} ${TARGET}
 
 sam build -t ${TEMPLATE}
