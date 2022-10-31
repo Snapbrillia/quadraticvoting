90e641eedd736b69ffec37c09797ea1c550ee138
ritterbush <ritterbush@berkeley.edu>
2022-10-31 09:11:06 -0700

    fix variables again

--- scripts/pay-fee.sh
+++ scripts/pay-fee.sh
@@ -1,9 +1,10 @@
 . scripts/env.sh
 
+scriptAddress=$(cat $scriptAddressFile)
 fee=$(qvf-cli collect-key-holder-fee)
 
 # TODO more string processing of fee is likely
 # Should be easy and straightforward to implement, below
 fee=$fee
 
-give_lovelace $scriptAddressFile $keyholder $fee
+give_lovelace $scriptAddress $keyHolder $fee
