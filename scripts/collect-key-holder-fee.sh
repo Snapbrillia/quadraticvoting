b8730607b6958edf31cea9aeacbc85ce640fb1b2
ritterbush <ritterbush@berkeley.edu>
2022-11-02 09:00:23 -0700

    some fixes

--- scripts/collect-key-holder-fee.sh
+++ scripts/collect-key-holder-fee.sh
@@ -1,7 +1,11 @@
+#!/bin/bash
+
 . scripts/env.sh
 
-scriptAddress=$(cat $scriptAddressFile)
-fee=$(qvf-cli collect-key-holder-fee)
+qvfAddress=$(cat $scriptAddressFile)
+govAsset=$(cat $govSymFile)
+
+fee=$(qvf-cli collect-key-holder-fee "$(cat $fileNamesJSONFile)")
 
 # TODO more string processing of fee is likely
 # Should be easy and straightforward to implement, below
