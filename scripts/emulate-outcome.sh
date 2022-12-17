bbc396ab020e73fe4cfdd783a95a0f28147b79e0
keyanmaskoot <keyanmaskoot@snapbrillia.com>
2022-12-17 14:52:19 +0100

    Minor fixes.

--- scripts/emulate-outcome.sh
+++ scripts/emulate-outcome.sh
@@ -1,4 +1,14 @@
-. $REPO/scripts/initiation.sh
+if [ -z $REPO ]; then
+  echo
+  echo "The \$REPO environment variable is not defined. Please review the script at"
+  echo "\`scripts/local-env.sh\` and make any desired changes, and then assign the"
+  echo "absolute path to this repository to \$REPO before proceeding."
+  return 1
+else
+  . $REPO/scripts/local-env.sh
+fi
+
+. $REPO/scripts/env.sh
 
 if [ "$1" == "--pretty" ]; then
   $qvf pretty-leaderboard "$(get_all_script_utxos_datums_values $(cat $scriptAddressFile))"
