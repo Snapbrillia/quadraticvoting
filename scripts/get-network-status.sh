e18ec626c5888d9396d7142d2f5c57afbc8aff83
Shan Zhang <shanzhang@Shans-MacBook-Air.local>
2023-01-05 21:11:48 -0500

    add script to calculate slot difference

---
+++ scripts/get-network-status.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -z $REPO ]; then
+  echo "The \$REPO environment variable is not defined. Please review the script at"
+  echo "\`scripts/local-env.sh\` and make any desired changes, and then assign the"
+  echo "absolute path to this repository to \$REPO before proceeding."
+  return 1
+else
+. $REPO/scripts/local-env.sh
+fi
+
+. $REPO/scripts/initiation.sh
+
+checkStatusOf=$1
+
+differenceBetweenSlots=$(get_slot_difference $checkStatusOf)
+
+echo $differenceBetweenSlots
