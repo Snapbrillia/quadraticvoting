dcb6c313fd3d0afeea9093137b59a6790f0e733c
Shan Zhang <shanzhang@Shans-MacBook-Air.local>
2023-01-18 11:45:49 -0500

    add success to end of donation queue

--- scripts/donate-to-project.sh
+++ scripts/donate-to-project.sh
@@ -302,7 +302,8 @@ else
   elif [ "$QUEUE" == "True" ]; then
     # {{{
     sign_and_submit_tx $custodialWalletsDir/$walletLabel.skey $preDir/$collateralKeyHolder.skey $preDir/$keyHolder.skey
-    store_current_slot $projectTokenName
+    store_current_slot_2 $projectTokenName 
+    echo "Success"
     # }}}
   else
     # {{{
