054a58b6e0414fbd3bd25247f022c5c69e8493f6
Shan Zhang <shanzhang@Shans-MacBook-Air.local>
2023-01-21 15:13:14 -0500

    store slot only when signing the transaction

---
+++ scripts/collateral-key-holder-sign-transaction.sh
@@ -0,0 +1,40 @@
+if [ -z $REPO ]; then
+  echo "error"
+  return 1
+else
+. $REPO/scripts/local-env.sh
+fi
+
+. $REPO/scripts/initiation.sh
+
+transactionCBOR=$1
+projectTokenName=$2
+
+if [ "$SIGNREGISTRATION" = 'True' ]; then
+  differenceBetweenSlots=$(get_slot_difference $scriptLabel)
+elif [ "$SIGNDONATION" = 'True' ]; then
+  differenceBetweenSlots=$(get_slot_difference $projectTokenName)
+fi
+
+if [ $differenceBetweenSlots -lt 100 ]; then
+  echo "NetworkBusy"
+else 
+  newJson=$(cat $txBody | jq -c --arg cbor "$transactionCBOR" '.cborHex = $cbor')
+  echo "$newJson" > $txBody
+  sign_tx_by $preDir/$collateralKeyHolder.skey
+  JSON_STRING=$( jq -n                                 \
+      --arg tu "$(cat $txSigned | jq -r .cborHex)"     \
+      '{signedTx: $tu}' )
+  echo "$JSON_STRING"
+  if [ "$SIGNREGISTRATION" = 'True' ]; then 
+    store_current_slot_2 $projectTokenName $scriptLabel
+  elif [ "$SIGNDONATION" = 'True' ]; then
+    store_current_slot $projectTokenName 
+  fi
+fi
+
+
+
+
+
+
