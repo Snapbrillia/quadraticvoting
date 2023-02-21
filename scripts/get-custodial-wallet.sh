db3fc22821a05303008d80904b8fefbec9958b80
Shan Zhang <shanzhang@Shans-MacBook-Air.local>
2023-02-21 09:48:39 -0500

    fix typo

--- scripts/get-custodial-wallet.sh
+++ scripts/get-custodial-wallet.sh
@@ -15,6 +15,6 @@ walletLabel=$1
 if [ -f $custodialWalletsDir/$walletLabel.addr ]; then
   cat $custodialWalletsDir/$walletLabel.addr
 else 
-  generate_wallet $custodialWalletLabel/$walletLabel
+  generate_wallet $custodialsWalletLabel/$walletLabel
   cat $custodialWalletsDir/$walletLabel.addr
 fi
